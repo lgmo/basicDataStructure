0ae54bd1ff8ea7862cf42d0e7d51635fb8494133
Gabriel Schneider <gas6@cin.ufpe.br>
2021-11-01 01:11:37 -0300

    rust stack

---
+++ rust/weird-stack/target/debug/deps/weird_stack-46dc43bb3873c3bf.d
@@ -0,0 +1,5 @@
+/home/gbrls/Programming/contrib/basicDataStructure/rust/weird-stack/target/debug/deps/weird_stack-46dc43bb3873c3bf: src/main.rs
+
+/home/gbrls/Programming/contrib/basicDataStructure/rust/weird-stack/target/debug/deps/weird_stack-46dc43bb3873c3bf.d: src/main.rs
+
+src/main.rs:
