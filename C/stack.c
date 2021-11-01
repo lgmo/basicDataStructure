6275770043b30791ff2019ab68ea684e910f1a09
Mateus Elias <meap@cin.ufpe.br>
2021-11-01 02:13:42 -0300

    Create stack.c

---
+++ C/stack.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct link{
+    int element;
+    struct link *next;
+} Link;
+
+typedef struct{
+    Link *top;
+    int size;
+} Stack;
+
+Link *create_link(int it, Link *nextval){
+    Link *n;
+    n = (Link*) malloc(sizeof(Link));
+    n->element = it;
+    n->next = nextval;
+    return n;
+}
+
+Stack *create_stack(){
+    Stack *s;
+    s = (Stack*) malloc(sizeof(Stack));
+    s->top = NULL;
+    s->size = 0;
+    return s;
+}
+
+void push(Stack *s, int it){
+    s->top = create_link(it, s->top);
+    s->size++;
+}
+
+int pop(Stack *s){
+    int it;
+    Link *n;
+    if(s->top == NULL){
+        return -1;
+    }
+    it = s->top->element;
+    n = s->top;
+    s->top = s->top->next ;
+    free(n);
+    s->size--;
+    return it;
+}
+
+void clear(Stack *s){
+    while(s->top != NULL){
+        pop(s);
+    }
+    free(s);
+}
+
+int topValue(Stack *s){
+    if(s->top == NULL){
+        return -1;
+    }
+    return s->top->element;
+}
+
+int lenght(Stack *s){
+    return s->size;
+}
+
+int main(){
+
+    return 0;
+}
